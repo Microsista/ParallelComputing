ff32453b41dfc393aef255b9126c92b3c66f312b
Karol Jampolski <kjampolski@gmail.com>
2021-10-14 20:02:54 +0200

    dgdfgd

--- HelloWorld/Sum.cu
+++ HelloWorld/Sum.cu
@@ -7,20 +7,21 @@
 using namespace std;
 
 __global__ void sum(unsigned char* input, unsigned char* output, int workSize, int width) {
+	auto& indexInGroup = threadIdx.x;
 	__shared__ unsigned char shared[numberOfThreads + 2 * blurRadius];
-	int globalIndex = threadIdx.x + blockIdx.x * blockDim.x;
-	int localIndex = threadIdx.x + blurRadius;
+	int globalIndex = indexInGroup + blockIdx.x * blockDim.x;
+	int localIndex = indexInGroup + blurRadius;
 	
 	if (globalIndex < workSize) {
 		auto loadToShared = [&] {
 			auto actual = [&] {
-				shared[localIndex] = input[globalIndex + threadIdx.x * 3];
+				shared[localIndex] = input[globalIndex + indexInGroup * 3];
 			};
 
 			auto halo = [&] {
-				if (threadIdx.x < blurRadius) {
-					/*shared[localIndex - blurRadius] = globalIndex + threadIdx.x * 3 >= blurRadius * 3 ? input[globalIndex + threadIdx.x * 3 - blurRadius * 3] : 0;
-					shared[localIndex + numberOfThreads] = globalIndex + threadIdx.x * 3 < (workSize - numberOfThreads * 3) ? input[globalIndex * threadIdx.x * 3 + numberOfThreads * 3] : 0;*/
+				if (indexInGroup < blurRadius) {
+					shared[indexInGroup] = input[max(0, globalIndex - blurRadius)];
+					shared[localIndex + numberOfThreads] = input[min(workSize, globalIndex + numberOfThreads)];
 				}
 			};
 
