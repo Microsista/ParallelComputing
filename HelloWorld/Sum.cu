de07b7f5b8a37512dd5e57da68276eba3ce2fcc3
Karol Jampolski <kjampolski@gmail.com>
2021-10-15 08:19:37 +0200

    improve

--- HelloWorld/Sum.cu
+++ HelloWorld/Sum.cu
@@ -11,13 +11,27 @@ __global__ void sum(unsigned char* d_color, unsigned char* d_colorBlurred, int n
 	if (thread_2D_pos.x >= numCols || thread_2D_pos.y >= numRows)
 		return;
 
+	float weights[] = {
+		0.05f, 0.09f, 0.12f, 0.15f, 0.16f, 0.15f, 0.12f, 0.09f, 0.05f
+	};
 	int color = 0;
 	for (int i = -blurRadius; i <= blurRadius; i++) {
 		for (int j = -blurRadius; j <= blurRadius; j++) {
-			int2 sample2DPos = make_int2(blockIdx.x * blockDim.x + threadIdx.x + j,
-				blockIdx.y * blockDim.y + threadIdx.y + i);
+			int x = j, y = i;
+			if (thread_2D_pos.x + j <= 0)
+				x = 0;
+			if (thread_2D_pos.x + j > numCols)
+				x = 0;
+			if (thread_2D_pos.y + i <= 0)
+				y = 0;
+			if (thread_2D_pos.y + i > numRows)
+				y = 0;
+
+			int2 sample2DPos = make_int2(blockIdx.x * blockDim.x + threadIdx.x + x,
+				blockIdx.y * blockDim.y + threadIdx.y + y);
 
 			int sample1DPos = sample2DPos.y * numCols + sample2DPos.x;
+			
 			color += d_color[sample1DPos];
 		}
 	}
